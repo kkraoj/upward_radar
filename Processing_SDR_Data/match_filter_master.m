bbd4e4f55651c81ef95263dfe54b988d23e64829
kkraoj <you@example.com>
2019-09-11 22:09:25 -0700

    made upsample and filtering work on superfolder level

--- Processing_SDR_Data/match_filter_master.m
+++ Processing_SDR_Data/match_filter_master.m
@@ -1,4 +1,4 @@
-clear all 
+clear all
 close all 
 clc
 
@@ -6,27 +6,28 @@ clc
 ref_chirp = '/home/krishna/upwardradar/Processing_SDR_Data/ref_chirp_upsampled_x8.dat';
 % ref_chirp = '/home/krishna/upwardradar/Processing_SDR_Data/ref_chirp.dat';
 
-directory = '/home/krishna/upwardradar/upsampling_experiment';
+dataFolder = '/media/krishna/Seagate Backup Plus Drive/upwardradar/arboretum_upsampled'; %
 % directory = '/home/krishna/upwardradar/20_may_2019_2humanssittinginline_100ft_NS';
-cd(directory);
-savefolder = '/home/krishna/upwardradar/upsampling_experiment';
+% cd(directory);
+savefolder = '/home/krishna/upwardradar/filtered/arboretum';
 if ~exist(savefolder, 'dir')
        mkdir(savefolder)
 end
 
-files = dir('*.dat');
+files = dir(sprintf('%s/**/*.dat',dataFolder));
 
-cd '/home/krishna/upwardradar/radar';
+% cd '/home/krishna/upwardradar/radar';
 for c = 1:length(files)
     inputname = fullfile(files(c).folder, files(c).name);
-    savename = fullfile(savefolder, sprintf('correlated_%02d.dat',c));
-    
+    timerecorded = files(c).folder; 
+    timerecorded = timerecorded(end-13:end-10);
+    savename = fullfile(savefolder, sprintf('%s_%03d.dat',timerecorded,c));
     command = sprintf('./xcorr-fast "%s" "%s" > "%s"',ref_chirp, inputname, savename);
     [status,cmdout] = system(command)
     if status ~=0
-        sprintf('[FAILED] %s',inputname);
+        sprintf('[FAILED] %s\n',inputname);
     else
-        sprintf('[INFO] file processed : %d',c);
+        sprintf('[INFO] file processed : %d\n',c);
     end
 end
 
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ upward radar project
 
 Processing workflow:
 
-1. upsample in `matlab` using `FFTinterp.m`
+1. upsample in `matlab` using `upsampledData.m`
 2. perform matched filtering by calling Keith's `C` code from `matlab` using `match_filter_master.m`. This file calls the `C` code on all files within a folder
 3. use `peak_averager.m` to calculate mean and standard deviation of all peaks within chirp
